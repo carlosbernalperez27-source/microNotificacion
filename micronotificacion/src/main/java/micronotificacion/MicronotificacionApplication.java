f5c4268927c10a28ab8ce0261ad0a0de8fd75e5d
carlosbernalperez27-source <carlos.bernal.perez27@gmail.com>
2026-02-08 01:05:21 -0300

    se crean carpetas mvn

---
+++ micronotificacion/src/main/java/micronotificacion/MicronotificacionApplication.java
@@ -0,0 +1,13 @@
+package micronotificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicronotificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicronotificacionApplication.class, args);
+	}
+
+}
---
+++ micronotificacion/src/test/java/micronotificacion/MicronotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package micronotificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicronotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
